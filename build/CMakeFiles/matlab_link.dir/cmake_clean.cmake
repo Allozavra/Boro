64988453cf987c754c99701ffcae540270fab6b5
Allozavra <alla.farhutdinova@ya.ru>
2026-03-23 23:18:37 +0300

    Model in simulink

---
+++ build/CMakeFiles/matlab_link.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matlab_link.dir/src/matlab_link_node.cpp.o"
+  "CMakeFiles/matlab_link.dir/src/matlab_link_node.cpp.o.d"
+  "matlab_link"
+  "matlab_link.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matlab_link.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/matlab_link.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for matlab_link.
+# This may be replaced when dependencies are built.
